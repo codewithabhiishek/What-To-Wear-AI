b830959123a9df628eace5a5353c05c1c0f41178
codewithabhiishek <socials.abhiishek@gmail.com>
2026-08-25 11:52:26 +0530

    feat: add Vercel Analytics integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "@/App.jsx";
 import "@/index.css";
 
@@ -8,5 +9,7 @@ import { ThemeProvider } from "@/lib/ThemeProvider";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider defaultTheme="system" storageKey="app-ui-theme">
     <App />
+    <Analytics />
   </ThemeProvider>
 );
+
